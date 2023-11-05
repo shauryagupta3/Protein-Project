61e92c723cfe3faadb063685ce0e5b7e4483c6f6
Shaurya Gupta <119103602+shauryagupta3@users.noreply.github.com>
2023-11-05 13:08:20 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const cors = require("cors");
 const express = require("express");
 var morgan = require("morgan");
+var nodemon = require("nodemon");
 const app = express();
 app.use(cors());
 app.use(express.json());
